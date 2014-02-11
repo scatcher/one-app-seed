9822e0bfa4e34eb7c25e2f8637066c9450ff0b2e
Scott <github@scotthatcher.com>
2014-02-10 17:57:54 -0800

    Removed OneApp ESED references

--- app/scripts/services/config.js
+++ app/scripts/services/config.js
@@ -2,17 +2,21 @@
 
 angular.module('OneApp')
     .service('config', ['$q', function config($q) {
+
         // AngularJS will instantiate a singleton by calling "new" on this function
-        var config = this;
         toastr.options.positionClass = 'toast-bottom-right';
         toastr.options.timeOut = 3000;
-        //Determines if offline data should be used instead of web service calls
+
+        //Determines if offline (development) data should be used instead of web service calls
         var offline = window.location.href.indexOf('.mil') === -1 && window.location.href.indexOf('.com') === -1;
+
+        //Prefix that when added to the account name evaluates to the full user login name
+        //Only needed if your instance adds a prefix
+//        var userLoginNamePrefix = 'i:0#.w|';
 
         var defaultUrl = $().SPServices.SPGetCurrentSite();
 
         return {
-            appTitle: 'ESED Dashboard',
             debugEnabled: true,
             defaultUrl: defaultUrl,
             offline: offline
